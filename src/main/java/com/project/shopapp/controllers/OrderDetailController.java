8b46abf5eb906e680ef902255da5d659760837f7
khnhk0ogei04 <khanh8a04@gmail.com>
2024-08-26 22:51:56 +0700

    update v3

--- src/main/java/com/project/shopapp/controllers/OrderDetailController.java
+++ src/main/java/com/project/shopapp/controllers/OrderDetailController.java
@@ -1,14 +1,26 @@
 package com.project.shopapp.controllers;
 
 import com.project.shopapp.dtos.OrderDetailDTO;
+import com.project.shopapp.exceptions.DataNotFoundException;
+import com.project.shopapp.models.OrderDetail;
+import com.project.shopapp.repositories.OrderDetailRepository;
+import com.project.shopapp.services.OrderDetailService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("${api.prefix}/order_details")
 public class OrderDetailController {
+    private final OrderDetailService orderDetailService;
+
+    public OrderDetailController(OrderDetailService orderDetailService) {
+        this.orderDetailService = orderDetailService;
+    }
+
     @PostMapping
     public ResponseEntity<?> createOrderDetail (
             @Valid @RequestBody OrderDetailDTO newOrderDetail
@@ -16,12 +28,14 @@ public class OrderDetailController {
         return ResponseEntity.ok(newOrderDetail);
     }
     @GetMapping("/{id}")
-    public ResponseEntity<?> getOrderDetail (@Valid @PathVariable("id") long id) {
-        return ResponseEntity.ok("getOrderDetail with Id " + id);
+    public ResponseEntity<?> getOrderDetail (@Valid @PathVariable("id") long id) throws DataNotFoundException {
+        OrderDetail orderDetail = orderDetailService.getOrderDetail(id);
+        return ResponseEntity.ok().body(orderDetail);
     }
     // Lay ra danh sach cac order_details cua 1 order nao do:
     @GetMapping("/order/{orderId}")
     public ResponseEntity<?> getOrderDetails(@Valid @PathVariable("orderId") long orderId) {
+        List<OrderDetail> orderDetails = orderDetailService.findByOrderId(orderId);
         return ResponseEntity.ok("getOrderDetails with orderId " + orderId);
     }
     // Sua doi 1 cai orderDetails nao do
---
+++ src/main/java/com/project/shopapp/services/OrderDetailService.java
@@ -0,0 +1,60 @@
+package com.project.shopapp.services;
+
+import com.project.shopapp.dtos.OrderDetailDTO;
+import com.project.shopapp.exceptions.DataNotFoundException;
+import com.project.shopapp.models.Order;
+import com.project.shopapp.models.OrderDetail;
+import com.project.shopapp.models.Product;
+import com.project.shopapp.repositories.OrderDetailRepository;
+import com.project.shopapp.repositories.OrderRepository;
+import com.project.shopapp.repositories.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+
+@RequiredArgsConstructor
+@Service
+public class OrderDetailService implements IOrderDetailService {
+    private final OrderRepository orderRepository;
+    private final ProductRepository productRepository;
+    private final OrderDetailRepository orderDetailRepository;
+    @Override
+    public OrderDetail createOrderDetail(OrderDetailDTO orderDetailDTO) throws DataNotFoundException {
+        Order order = orderRepository.findById(orderDetailDTO.getOrderId())
+                .orElseThrow(() -> new DataNotFoundException("Can't find Order with ID: " + orderDetailDTO.getOrderId()));
+        // Tim product theo id:
+        Product product = productRepository.findById(orderDetailDTO.getOrderId()).orElseThrow(() -> new DataNotFoundException("Can't find Product with ID: " + orderDetailDTO.getOrderId()));
+        OrderDetail orderDetail = OrderDetail.builder()
+                .order(order)
+                .product(product)
+                .numberOfProducts(orderDetailDTO.getNumberOfProducts())
+                .price(orderDetailDTO.getPrice())
+                .totalMoney(orderDetailDTO.getTotalMoney())
+                .color(orderDetailDTO.getColor())
+                .build();
+        //lưu vào db
+        return orderDetailRepository.save(orderDetail);
+    }
+
+    @Override
+    public OrderDetail getOrderDetail(Long id) throws DataNotFoundException {
+        return orderDetailRepository.findById(id).orElseThrow(() -> new DataNotFoundException("Can't find Order with ID: " + id));
+    }
+
+    @Override
+    public OrderDetail updateOrderDetail(Long id, OrderDetailDTO orderDetailDTO) {
+        return null;
+    }
+
+    @Override
+    public void deleteOrderDetail(Long id) {
+        orderDetailRepository.deleteById(id);
+    }
+
+    @Override
+    public List<OrderDetail> findByOrderId(Long orderId) {
+        return orderDetailRepository.findByOrderId(orderId);
+    }
+}
